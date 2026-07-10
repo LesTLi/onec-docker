cce4770a489d4f5714a3797d8a057e4b0b47944e
DmitryAlman <156434721+DmitryAlman@users.noreply.github.com>
2026-07-10 14:01:26 +1000

    Update build_base_test-utils.sh

--- build_base_test-utils.sh
+++ build_base_test-utils.sh
@@ -75,6 +75,7 @@ docker build \
     --build-arg DOCKER_REGISTRY_URL=$DOCKER_REGISTRY_URL \
     --build-arg BASE_IMAGE=onec-client-vnc-oscript-jdk \
     --build-arg BASE_TAG=$ONEC_VERSION \
+	--build-arg ONEC_VERSION=$ONEC_VERSION \
     -t $DOCKER_REGISTRY_URL/onec-client-vnc-oscript-jdk-testutils:$ONEC_VERSION \
     -f test-utils/Dockerfile \
     $last_arg
